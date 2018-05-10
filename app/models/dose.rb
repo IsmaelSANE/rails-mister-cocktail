a2f30865a90de16fdbcd0ed36ba7d137f7d686da
Ismael Sane <six.echo.sa@gmail.com>
2018-05-10 15:15:10 +0200

    late

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,7 +1,5 @@
 class Dose < ApplicationRecord
   belongs_to :cocktail
   belongs_to :ingredient
-  validates :description, uniqueness: true
-  validates_uniqueness_of :cocktail, :ingredient
-  validates_uniqueness_of :cocktail, :scope => [:ingredient]
+  validates :cocktail, uniqueness: { scope: :ingredient }
 end
